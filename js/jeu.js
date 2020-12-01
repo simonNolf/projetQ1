719965f46a45fd073bb0cb3947fbcc1b3deca758
simonNolf <63592563+simonNolf@users.noreply.github.com>
2020-12-01 09:10:58 +0100

    base

---
+++ js/jeu.js
@@ -0,0 +1,9 @@
+"use strict";
+let jeux_pc= ["Leagues of Legends", "Heartstones", "fortnite", "Apex", "GTA", "Smite", "Minecraft"];
+let jeux_ps = ["GTA", "fortinte", "Crash", "Assassin's Creed", "FIFA", "Minecraft", "Coll of Duty"];
+function jeu_pc(jeux_pc){
+
+}
+function jeu_ps(jeux_ps){
+
+}
